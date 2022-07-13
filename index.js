45a08aad2aa001710f4c7d454360118064232848
Mustafa Toprak <damonatabay@gmail.com>
2022-07-13 15:56:21 +0200

    daily change

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const cors = require('cors');
 const app = express();
 const importData = require('./app.json');
 let port = process.env.PORT || 3005;
+let today = new Date();
+let yesterday = new Date('July 12, 2022');
+
+let hadithNumber = 0;
+
+if (today !== yesterday) {
+  console.log('hello');
+  yesterday = today;
+  today = new Date();
+  hadithNumber = hadithNumber + 1;
+
+  if (hadithNumber === 3) {
+    hadithNumber = 0;
+  }
+}
 
 app.use(cors());
 
@@ -11,7 +26,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/hadith', (req, res) => {
-  res.send(importData);
+  res.send(importData[hadithNumber]);
 });
 
 app.listen(port, () => {
