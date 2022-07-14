8ccbd94303fbb1e82d02d7a9309a640bc52006f0
Mustafa Toprak <damonatabay@gmail.com>
2022-07-14 15:50:03 +0200

    changed the logic

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ const app = express();
 const importData = require('./app.json');
 let port = process.env.PORT || 3005;
 let today = new Date();
-let yesterday = new Date('July 12, 2022');
-
-let hadithNumber = 0;
-
-if (today !== yesterday) {
-  console.log('hello');
-  yesterday = today;
-  today = new Date();
-  hadithNumber = hadithNumber + 1;
-
-  if (hadithNumber === 3) {
-    hadithNumber = 0;
-  }
-}
 
 app.use(cors());
 
@@ -25,12 +11,8 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/test', (req, res) => {
-  res.send('today', today, 'yesterday', yesterday);
-});
-
 app.get('/hadith', (req, res) => {
-  res.send(importData[hadithNumber]);
+  res.send(importData[today.getDay()]);
 });
 
 app.listen(port, () => {
